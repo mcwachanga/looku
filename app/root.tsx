4ba24bb4b4ac0f8ec5ebd7f074ea748b75a9731c
Brian Wachanga <b.wachanga@gmail.com>
2026-02-09 21:42:05 +0300

    update: install shadcn and tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css?url"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return (
